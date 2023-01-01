bd9c39204266520fb2e88ae7939a1dbe3b9cfbcc
unknown <rillomsholeh>
2023-01-01 21:31:52 +0700

    udpate

--- src/pages/Index.js
+++
@@ -1,7 +0,0 @@
-// import "pages" component here
-import Home from "./Home";
-import About from "./About";
-import Profile from "./Profile";
-import SignIn from "./SignIn";
-
-export { Home, About, Profile, SignIn };
