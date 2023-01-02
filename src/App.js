503cda2cb442e8df3e459e1263965f34019a2d7e
unknown <rillomsholeh>
2023-01-02 21:19:29 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,41 +1,9 @@
-// import "bootstrap/dist/css/bootstrap.min.css";
+import React from 'react'
 
-
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Profile from "./pages/Profile";
-import SignIn from "./pages/SignIn";
-
-function App() {
+const App = () => {
   return (
-    <Router>
-      <div>
-        {/* Setup navigation element */}
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/profile">Profile</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/about" element={<About />} />
-        <Route exact path="/profile" element={<Profile />} />
-        <Route exact path="/signin" element={<SignIn />} />
-      </Routes>
-    </Router>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
